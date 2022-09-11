010c71347acd5ca2f3aaf6a29673bc427ef02718
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-11 19:32:33 +0300

    completed the example

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Введите целое число a: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число b: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число c: ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = Convert.ToInt32(a);
+
+if(b > a)
+{
+     max = b;
+} 
+if(c > max)
+{
+    max = c;
+}
+ Console.WriteLine("Максимальное число: " + max);
