9dd97adc53df32ce33a478b52bedea57201b4f62
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-12 12:56:26 +0300

    example  Task_003 solved

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine(a + " Является четным числом");
+} 
+else
+{
+    Console.WriteLine(a + " Является не четным числом");
+}
